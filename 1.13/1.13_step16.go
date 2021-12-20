faf65fa8e4f1d1175afdc37de097720b45b57dc4
Sergey Matveev <s.matveev.991@gmail.com>
2021-12-20 19:32:54 +0200

    added solution 1.13(11-16)

---
+++ 1.13/1.13_step16.go
@@ -0,0 +1,36 @@
+//Из натурального числа удалить заданную цифру.
+//
+//Входные данные
+//
+//Вводятся натуральное число и цифра, которую нужно удалить.
+//
+//Выходные данные
+//
+//Вывести число без заданных цифр.
+//
+//Sample Input:
+//
+//38012732
+//3
+//Sample Output:
+//
+//801272
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var n, d int
+	_, err := fmt.Scan(&n, &d)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(strings.Replace(strconv.Itoa(n), strconv.Itoa(d), "", -1))
+}
